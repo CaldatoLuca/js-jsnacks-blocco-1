4700ddb8c65264de5d01607c6fffe6d141e1944c
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-10 11:24:30 +0100

    snack 2

--- snack-2/js/main.js
+++ snack-2/js/main.js
@@ -1,2 +1,25 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// inserico le due parole con prompt
+// controloo con if quale sia la piu lunga (stampo la piu corta) else (stampo l' altra)
+
+`use.strict`;
+
+//? variabili
+const parolaUno = prompt("Inserisci qui la parola uno");
+const parolaDue = prompt("Inserisci qui la parola due");
+
+//? controllo in console dei valori inseriti
+console.log({ parolaUno });
+console.log({ parolaDue });
+console.log(parolaUno.length);
+console.log(parolaDue.length);
+
+if (parolaUno.length > parolaDue.length) {
+  alert(parolaUno);
+} else if (parolaUno.length < parolaDue.length) {
+  alert(parolaDue);
+} else {
+  alert(`${parolaDue} è lunga come ${parolaUno}`);
+}
