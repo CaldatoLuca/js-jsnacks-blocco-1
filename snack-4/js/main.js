d8a5cfd6574bf53644df9e22cbf440f42559382d
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-14 11:49:18 +0100

    snack 4

--- snack-4/js/main.js
+++ snack-4/js/main.js
@@ -6,6 +6,7 @@
 const listaNumeri = [];
 let somma = 0;
 
+/*
 do {
   const numero = prompt("Inserisci un numero");
   if (!isNaN(numero)) {
@@ -18,6 +19,7 @@ do {
 
 console.log(`La tua lista di numeri è: ${listaNumeri}`);
 console.log(`La somma dei tuoi numeri è: ${somma}`);
+*/
 
 /*
 while (somma < 50) {
@@ -33,17 +35,15 @@ console.log(`La tua lista di numeri è: ${listaNumeri}`);
 console.log(`La somma dei tuoi numeri è: ${somma}`);
 */
 
-/*
- for (let i = 0; i < Infinity; i++) {
-   if (somma < 50) {
-     const numero = +prompt("Inserisci un numero");
-     listaNumeri.push(numero);
-     somma += numero;
-   } else {
-     break;
-   }
- }
+for (let i = 0; i < Infinity; i++) {
+  if (somma < 50) {
+    const numero = +prompt("Inserisci un numero");
+    listaNumeri.push(numero);
+    somma += numero;
+  } else {
+    break;
+  }
+}
 
- console.log(`La tua lista di numeri è: ${listaNumeri}`);
- console.log(`La somma dei tuoi numeri è: ${somma}`);
-*/
+console.log(`La tua lista di numeri è: ${listaNumeri}`);
+console.log(`La somma dei tuoi numeri è: ${somma}`);
